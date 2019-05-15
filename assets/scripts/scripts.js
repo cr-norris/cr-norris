ef6f98fa378be6592576f5ff92cce91a34e77c97
Christopher Norris <chris@alder.studio>
2019-05-15 09:21:02 -0400

    Stoned

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    
+    var width = $(window).width();
+    var height = $(window).height();
+
+    // random X
+    let max_x = width;
+    let min_x = 0;
+    let random_x = Math.floor(Math.random() * (+max_x - +min_x)) + +min_x;
+
+    // random Y
+    let max_y = height;
+    let min_y = 0;
+    let random_y = Math.floor(Math.random() * (+max_y - +min_y)) + +min_y;
+
+
+    $(".stone").draggable();
+    $(".stone").css({
+        right: random_x,
+        bottom: random_y
+    });
+
+});
+
+
